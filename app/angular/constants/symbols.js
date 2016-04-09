ddfd0966d456c4e205e1b687b2311c2b18bb4450
manbropig <jamie@thestorefront.com>
2016-04-08 17:51:14 -0700

    Create a Symbols Constant to help DRY out code.
    This way we know all of the keys and their types at
    any level of depth, from controller to leaf node directives
    as long as we inject the Symbols constant.

---
+++ app/angular/constants/symbols.js
@@ -0,0 +1,12 @@
+app.constant('Symbols', {
+  numbers: '7894561230'.split('').map(function(num) {
+    return { value: num, keyType: 'number' };
+  }),
+  modifiers: 'AC +/- %'.split(' ').map(function(num) {
+    return { value: num, keyType: 'modifier' };
+  }),
+  operations: '/x-+='.split('').map(function(num) {
+    return { value: num, keyType: 'operation' };
+  }),
+  decimal:  { value: '.', keyType: 'decimal' }
+});
