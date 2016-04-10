fc9d28fd409a437ae97a782fa9e4198ee8752689
manbropig <jamie@thestorefront.com>
2016-04-10 11:25:41 -0700

    Rename Variables to Make More Sense

--- app/angular/constants/symbols.js
+++ app/angular/constants/symbols.js
@@ -2,11 +2,11 @@ app.constant('Symbols', {
   numbers: '7894561230'.split('').map(function(num) {
     return { value: num, keyType: 'number' };
   }),
-  modifiers: 'AC +/- %'.split(' ').map(function(num) {
-    return { value: num, keyType: 'modifier' };
+  modifiers: 'AC +/- %'.split(' ').map(function(mod) {
+    return { value: mod, keyType: 'modifier' };
   }),
-  operations: '/x-+='.split('').map(function(num) {
-    return { value: num, keyType: 'operation' };
+  operations: '/x-+='.split('').map(function(op) {
+    return { value: op, keyType: 'operation' };
   }),
   decimal:  { value: '.', keyType: 'decimal' },
   OPS_REGEX: /[-()\+\-*\/]/g
